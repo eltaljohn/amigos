84fc97c5f5f0876d554017a6ededc98a23ce3b2e
john.bolanos <john.bolanos@olive.com>
2023-05-31 19:00:31 -0500

    first part about rooms implementation

--- js/db.js
+++ js/db.js
@@ -1,32 +1,27 @@
-const saveGame = (game) => {
-  try {
-    const data = getData();
-    if (data) {
-      const g = data.db.find((x) => x.id === game.id);
-      if (!g) {
-        data.db.push(game);
-        saveData(`{"db":${JSON.stringify(data.db)}}`);
-      }else{
-        const games = data.db.filter((x) => x.id !== game.id);  
-        games.push(game);  
-        saveData(`{"db":${JSON.stringify(games)}}`);
-      }
-    } else {
-      localStorage.setItem("db", `{"db":[${JSON.stringify(game)}]}`);
-    }
-  } catch (e) {
-    console.error(e);
+const saveGame = (game, roomID) => {
+  const data = getData();
+  if (data[roomID]) {
+    const g = data[roomID].games.find((x) => x.id === game.id);
+    if (!g) {
+      data[roomID].games.push(game);
+      saveData(data);
+    } 
+    // else {
+    //   const games = data[roomID].games.filter((x) => x.id !== game.id);
+    //   games.push(game);
+    //   saveData(`{"db":${JSON.stringify(games)}}`);
+    // }
   }
 };
 
 const saveMoves = (moves, id) => {
   try {
     const game = getGame(id);
-    saveGame({...game, moves})
+    saveGame({ ...game, moves });
   } catch (e) {
     console.error(e);
   }
-}
+};
 
 const getGame = (id) => {
   const data = getData();
@@ -36,8 +31,14 @@ const getGame = (id) => {
   console.error("game id not found");
 };
 
-const saveData = (db) => localStorage.setItem("db", db);
+const saveData = (db) => localStorage.setItem("db", JSON.stringify(db));
 
 const getData = () => JSON.parse(localStorage.getItem("db"));
 
-export { saveGame, getData, getGame, saveMoves };
+const createNewRoom = (roomID) => {
+  const db = getData();
+  const newDB = { ...db, [roomID]: { createdDate: Date.now(), games: [] } };
+  saveData(newDB);
+};
+
+export { saveGame, getData, getGame, saveMoves, createNewRoom };
